99c0a4322201484cae9d7a8463319af096323d5c
Sean Harte <scsharte@gmail.com>
2018-09-01 04:18:20 -0400

    removed cdn and imported pure css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LandingPage from './components/landing/landing';
+import DashBoard from './components/dashboard/dashboard';
+import 'purecss/build/pure.css';
+import 'purecss/build/grids-responsive-min.css';
+import 'purecss/build/grids-responsive-old-ie-min.css';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +13,7 @@ class App extends Component {
             <Router>
                 <div>
                     <Route exact path="/" component={ LandingPage } />
+                    <Route exact path="/dashboard" component={ DashBoard } />
                 </div>
             </Router>
         );
