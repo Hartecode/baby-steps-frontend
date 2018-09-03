862bab0a4c221005ddf8667aeabe0f7d0994bf0e
Sean Harte <scsharte@gmail.com>
2018-09-02 22:45:43 -0400

    added new/modified actions

---
+++ src/actions/protected-data.js
@@ -0,0 +1,32 @@
+
+import { SERVER_BASE_API } from '../config';
+import { normalizeResponseErrors } from './utils';
+
+export const FETCH_PROTECTED_DATA_SUCCESS = 'FETCH_PROTECTED_DATA_SUCCESS';
+export const fetchProtectedDataSuccess = data => ({
+    type: FETCH_PROTECTED_DATA_SUCCESS,
+    data
+});
+
+export const FETCH_PROTECTED_DATA_ERROR = 'FETCH_PROTECTED_DATA_ERROR';
+export const fetchProtectedDataError = error => ({
+    type: FETCH_PROTECTED_DATA_ERROR,
+    error
+});
+
+export const fetchProtectedData = () => (dispatch, getState) => {
+    const authToken = getState().auth.authToken;
+    return fetch(`${SERVER_BASE_API}/protected`, {
+        method: 'GET',
+        headers: {
+            // Provide our auth token as credentials
+            Authorization: `Bearer ${authToken}`
+        }
+    })
+        .then(res => normalizeResponseErrors(res))
+        .then(res => res.json())
+        .then(({data}) => dispatch(fetchProtectedDataSuccess(data)))
+        .catch(err => {
+            dispatch(fetchProtectedDataError(err));
+        });
+};
