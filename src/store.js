cf65e95a763ded17cf2c3bb7bca1716dc3121441
Sean Harte <scsharte@gmail.com>
2018-09-02 22:51:09 -0400

    added midderware

--- src/store.js
+++ src/store.js
@@ -1,8 +1,27 @@
-import { createStore,  applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
+import { loadAuthToken } from './local-storage';
+import authReducer from './reducers/auth';
+import protectedDataReducer from './reducers/protected-data';
 import Reducer from './reducers/index';
+import { setAuthToken, refreshAuthToken } from './actions/auth';
 
-export default createStore(
-	Reducer	, 
-	applyMiddleware(thunk)
+const store = createStore(
+    combineReducers({
+        form: formReducer,
+        auth: authReducer,
+        protectedData: protectedDataReducer
+    }),
+    applyMiddleware(thunk)
 );
+
+// Hydrate the authToken from localStorage if it exist
+const authToken = loadAuthToken();
+if (authToken) {
+    const token = authToken;
+    store.dispatch(setAuthToken(token));
+    store.dispatch(refreshAuthToken());
+}
+
+export default store;
