682d14a0c3bb85f320eba529996c8cecf45c2f96
Sean Harte <scsharte@gmail.com>
2018-09-02 22:44:18 -0400

    added more to config

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const SERVER_BASE_API = process.env.APP_API_BASE_URL || "http://localhost:8080"
+export const SERVER_BASE_API = process.env.APP_API_BASE_URL || "http://localhost:8080";
+export const DEMO_USER_NAME = 'bacon4ever';
+export const DEMO_PASSWORD = 'notagoodpassword';
